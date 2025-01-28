e82a7fd221671d786d716d22421eccd25142e0dc
vrishabh081 <rishabhvishwakarma398@gmail.com>
2025-01-28 20:50:12 +0530

    Basic review of config folder done

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,11 +2,10 @@
 import { initializeApp } from "firebase/app";
 import { getStorage} from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { getMessaging } from "firebase/messaging";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
